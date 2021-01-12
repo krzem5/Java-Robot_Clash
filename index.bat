72bdb36d62ca1c8bb293d8d045d6870fe46dd7c2
Krzem <krzem5.dev@gmail.com>
2021-01-12 20:24:08 +0100

    Push Update 01/12/2021, 20:03:43

--- index.bat
+++ index.bat
@@ -1,5 +1,21 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac com/krzem/robot_clash/Main.java&&java com/krzem/robot_clash/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+if exist build rmdir /s /q build
+mkdir build
+cd src
+javac -d ../build com/krzem/robot_clash/Main.java&&jar cvmf ../manifest.mf ../build/robot_clash.jar -C ../build *&&goto run
+cd ..
+goto end
+:run
+cd ..
+pushd "build"
+for /D %%D in ("*") do (
+	rd /S /Q "%%~D"
+)
+for %%F in ("*") do (
+	if /I not "%%~nxF"=="robot_clash.jar" del "%%~F"
+)
+popd
+cls
+java -jar build/robot_clash.jar
+:end
