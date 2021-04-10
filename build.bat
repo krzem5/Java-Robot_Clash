dded130fda1662adad55ec4696e02f0d1a5a3513
Krzem <krzem5.dev@gmail.com>
2021-04-10 10:52:47 +0200

    Push Update 04/10/2021, 10:52:28

--- build.bat
+++ build.bat
@@ -1,21 +1,3 @@
 @echo off
 cls
-if exist build rmdir /s /q build
-mkdir build
-cd src
-javac -d ../build com/krzem/robot_clash/Main.java&&jar cvmf ../manifest.mf ../build/robot_clash.jar -C ../build *&&goto run
-cd ..
-goto end
-:run
-cd ..
-pushd "build"
-for /D %%D in ("*") do (
-	rd /S /Q "%%~D"
-)
-for %%F in ("*") do (
-	if /I not "%%~nxF"=="robot_clash.jar" del "%%~F"
-)
-popd
-cls
-java -jar build/robot_clash.jar
-:end
+python build.py %*
